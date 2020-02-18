9bc685b7002605fa9faa52208d4d7807656058e0
Sebastian Wojsz <s.wojsz@gmail.com>
2020-02-18 22:36:01 +0100

    Zadanie - rozbudowa kodu ver. 1.2

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -6,8 +6,10 @@ public class LeapYear {
         int year;
         System.out.println("Algorytm sprawdza czy rok jest przestępny");
         System.out.println("Podaj rok: ");
-        Scanner sc = new Scanner(System.in);
-        year = sc.nextInt();
+
+        Scanner leapYear;
+        leapYear= new Scanner(System.in);
+        year = leapYear.nextInt();
         if(year%4==0 && year%100!=0 || year%400==0){
             System.out.println("Rok przestępny!");
         }
